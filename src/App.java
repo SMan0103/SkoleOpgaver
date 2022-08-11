94af242a1ac01168dabdd0788e540d5252dcdf12
Emil Thostrup Pedersen <steamman0103@gmail.com>
2022-08-11 11:21:31 +0200

    first commit

---
+++ src/App.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+import javax.sound.sampled.FloatControl;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner hej2 = new Scanner(System.in);
+        int hej = hej2.nextInt();
+        
+
+        for (int i = 0; i < 21; i++) {
+            int i2 = i * hej;
+
+            System.out.println(i2);
+        }
+
+    }
+}
+
+
+
+
---
+++ src/Opgave4.java
@@ -0,0 +1,43 @@
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+ 
+
+class Opgave4 {
+ 
+    // Main driver method
+    public static void main(String[] args) throws Exception
+    {
+
+        Scanner hej5 = new Scanner(System.in);
+        
+
+        String str = hej5.nextLine();
+       
+  
+        char arr[] = str.toCharArray();
+ 
+       
+        char temp;
+ 
+        int i = 0;
+        while (i < arr.length) {
+            int j = i + 1;
+            while (j < arr.length) {
+                if (arr[j] < arr[i]) {
+                   
+
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+                j += 1;
+            }
+            i += 1;
+        }
+ 
+    
+        System.out.println(arr);
+    }
+}
